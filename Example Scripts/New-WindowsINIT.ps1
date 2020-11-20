8b2fc8c8b2ba281ea7f1a6799c3032ddc843bee0
JayAreP <43276115+JayAreP@users.noreply.github.com>
2020-11-20 10:39:14 -0700

    Update New-WindowsINIT.ps1

--- Example Scripts/New-WindowsINIT.ps1
+++ Example Scripts/New-WindowsINIT.ps1
@@ -1,21 +1,28 @@
 param(
     [parameter(Mandatory)]
     [string] $Data1Interface,
-    [parameter(Mandatory)]
+    [parameter()]
     [string] $Data2Interface,
     [parameter()]
     [switch] $initialize,
     [parameter()]
     [switch] $startiSCSI,
     [parameter()]
-    [switch] $addHW
+    [switch] $addHW,
+    [parameter()]
+    [switch] $addHost
 )
 <#
     .SYNOPSIS
         This Script will automatically create and fullfill the iSCSI connection on any windows host by automatically querying the SDP data interfaces.
+        You can argue for operations like:
+            -addHost -- This will add the host object to the SDP with the local iqn.
+            -addHW -- For the first time you run the script, this will add the Silk hardware vendor values to the HW list. 
+            -startiSCSI -- This will start the iSCSI service and set it for automatic start. 
+            -initialize -- This will initialilze the newly provisioned LUNs and put a drive letter on them for immediate use. 
 
     .EXAMPLE
-        New-WIndowsINIT.ps1 -Data1Interface Data1 -Data2Interface Data2
+        New-WIndowsINIT.ps1 -Data1Interface Data1 -Data2Interface Data2 -addHost -startiSCSI -addHW -initialize
 
 #>
 
@@ -28,9 +35,17 @@ if ($addHW) {
     New-MSDSMSupportedHW -VendorID KMNRIO -Product KDP
 }
 
+if ($addHost) {
+    $INITPort = Get-InitiatorPort
+    New-SDPHost -name $env:computername -type Windows
+    Set-SDPHostIqn -hostName $env:computername -iqn $INITPort.NodeAddress
+}
 
 $iSCSIData1 = Get-NetIPAddress -InterfaceAlias $Data1Interface -AddressFamily ipv4
-$iSCSIData2 = Get-NetIPAddress -InterfaceAlias $Data2Interface -AddressFamily ipv4
+if ($Data2Interface) {
+    $iSCSIData2 = Get-NetIPAddress -InterfaceAlias $Data2Interface -AddressFamily ipv4
+}
+
 
 $dataPorts = Get-SDPSystemNetPorts | where-object {$_.name -match "data"}
 
@@ -42,9 +57,11 @@ foreach ($i in $dataPorts) {
         $SDPIQN = Get-IscsiTarget
         Connect-IscsiTarget -NodeAddress $SDPIQN.NodeAddress -TargetPortalAddress $currentInt.ip_address -TargetPortalPortNumber 3260 -InitiatorPortalAddress $iSCSIData1.IPAddress -IsPersistent $true -IsMultipathEnabled $true
     } elseif ($i.name -like "*02") {
-        New-IscsiTargetPortal -TargetPortalAddress $currentInt.ip_address -TargetPortalPortNumber 3260 -InitiatorPortalAddress $iSCSIData2.IPAddress
-        $SDPIQN = Get-IscsiTarget
-        Connect-IscsiTarget -NodeAddress $SDPIQN.NodeAddress -TargetPortalAddress $currentInt.ip_address -TargetPortalPortNumber 3260 -InitiatorPortalAddress $iSCSIData2.IPAddress -IsPersistent $true -IsMultipathEnabled $true
+        if ($Data2Interface) {
+            New-IscsiTargetPortal -TargetPortalAddress $currentInt.ip_address -TargetPortalPortNumber 3260 -InitiatorPortalAddress $iSCSIData2.IPAddress
+            $SDPIQN = Get-IscsiTarget
+            Connect-IscsiTarget -NodeAddress $SDPIQN.NodeAddress -TargetPortalAddress $currentInt.ip_address -TargetPortalPortNumber 3260 -InitiatorPortalAddress $iSCSIData2.IPAddress -IsPersistent $true -IsMultipathEnabled $true
+        }
     }
 }
 
