1570fcb8751dcf7ac81e3525d688211df6333716
J.R. Phillips <43276115+JayAreP@users.noreply.github.com>
2024-08-06 07:30:36 -0700

    Add new replication snapshot function

--- SDP/CoreOps/VolumeGroupSnapshot/New-SDPReplicationVolumeGroupSnapshot.ps1
+++ SDP/CoreOps/VolumeGroupSnapshot/New-SDPReplicationVolumeGroupSnapshot.ps1
@@ -1,16 +1,14 @@
-function New-SDPVolumeGroupSnapshot {
+function New-SDPReplicationVolumeGroupSnapshot {
     param(
-        [parameter(Mandatory)]
-        [string] $name,
         [parameter(Mandatory,ValueFromPipelineByPropertyName)]
         [alias('pipeName')]
         [string] $volumeGroupName,
-        [parameter(Mandatory)]
-        [string] $retentionPolicyName,
         [parameter()]
         [switch] $deletable,
         [parameter()]
         [switch] $exposable,
+        [parameter(Mandatory)]
+        [string] $replicationSession,
         [parameter()]
         [string] $k2context = 'k2rfconnection'
     )
@@ -25,18 +23,19 @@ function New-SDPVolumeGroupSnapshot {
         $volumeGroupObject = Get-SDPVolumeGroup -name $volumeGroupName -k2context $k2context
         $volumeGroupPath = ConvertTo-SDPObjectPrefix -ObjectPath 'volume_groups' -ObjectID $volumeGroupObject.id -nestedObject
 
-        $retentionPolicyObject = Get-SDPRetentionPolicy -name $retentionPolicyName -k2context $k2context
-        $retentionPolicyPath = ConvertTo-SDPObjectPrefix -ObjectPath 'retention_policies' -ObjectID $retentionPolicyObject.id -nestedObject
-
         $o = New-Object psobject
         $o | Add-Member -MemberType NoteProperty -Name "short_name" -Value $name
         $o | Add-Member -MemberType NoteProperty -Name "source" -Value $volumeGroupPath
-        $o | Add-Member -MemberType NoteProperty -Name "retention_policy" -Value $retentionPolicyPath
         if ($deletable) {
             $o | Add-Member -MemberType NoteProperty -Name "is_auto_deleteable" -Value $true
         }
         if ($exposable) {
             $o | Add-Member -MemberType NoteProperty -Name "is_exposable" -Value $true
+        }
+        if ($replicationSession) {
+            $session = Get-SDPReplicationSessions -name $replicationSession -k2context $k2context
+            $sessionObj = ConvertTo-SDPObjectPrefix -ObjectPath 'replication/sessions' -ObjectID $session.id -nestedObject
+            $o | Add-Member -MemberType NoteProperty -Name "replication_session" -Value $sessionObj
         }
 
         $body = $o
